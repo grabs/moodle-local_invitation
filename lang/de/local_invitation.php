141f68022d8c53c5d400ce4e2822baf5b61277bd
Andreas Grabs <moodle@grabs-edv.de>
2020-06-25 21:48:15 +0200

    grabs-edv local_invitation: doc fixes and add german lang pack

---
+++ lang/de/local_invitation.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    local_invitation
+ * @author     Andreas Grabs <info@grabs-edv.de>
+ * @copyright  2020 Andreas Grabs EDV-Beratung
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['available_from'] = 'Verfügbar ab:';
+$string['available_to'] = 'Verfügbar bis:';
+$string['current_invitation'] = 'Aktuelle Einladung';
+$string['current_invitation_note'] = 'Wenn Sie eine neue Einladung erstellen, wird die bisherige automatisch ungültig.';
+$string['error_could_not_create_and_enrol'] = 'Fehler! Der neue Nutzer konnte nicht erstellt oder eingeschrieben werden.';
+$string['error_invalid_invitation'] = 'Fehler! Diese Einladung ist ungültig.';
+$string['error_invitation_not_active'] = 'Fehler! Das Einladungssystem ist auf diesem System nicht aktiviert.';
+$string['error_timeend_can_not_be_before_timestart'] = 'Das Verfügbarkeitsende kann nicht vor dem Beginn liegen.';
+$string['error_timestart_can_not_be_in_past'] = 'Die Einladung kann nicht in der Vergangenheit starten.';
+$string['free_slots'] = 'Freie Plätze:';
+$string['info_already_loggedin'] = 'Sie sind bereits angemeldet. Wenn Sie fortfahren, werden Sie automatisch abgemeldet.';
+$string['invitation'] = 'Einladung';
+$string['invitation:manage'] = 'Einladungen verwalten';
+$string['invitation_created'] = 'Die Einladung wurde erfolgreich erstellt.';
+$string['invite_participants'] = 'Teilnehmer einladen';
+$string['join'] = 'Teilnehmen';
+$string['max_users'] = 'Maximale Anzahl Teilnehmer';
+$string['max_users_per_invitation'] = 'Maximale Anzahl Teilnehmer je Einladung';
+$string['note'] = 'Hinweis:';
+$string['pluginname'] = 'Einladung';
+$string['remove_old_invitations'] = 'Alte Nutzer entfernen';
+$string['timeend'] = 'Ende';
+$string['timestart'] = 'Start';
+$string['userrole'] = 'Benutzerrolle';
+$string['welcome_note'] = 'Herzlich Willkommen {$a->firstname} {$a->lastname}!';
